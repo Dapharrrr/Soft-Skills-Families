09334ab63f27c82e42a0195ac44d124bde25d88f
Dapharrrr <dubost.raphael1@gmail.com>
2024-12-04 14:02:36 +0100

    feat: :sparkles: move assets to public

---
+++ src/data.tsx
@@ -0,0 +1,405 @@
+export const bonusCards = [
+    // Communication Family
+    {
+      id: 1,
+      title: "Communication Family",
+      description:
+        "Can you explain what active listening means, using as many words as possible?",
+    },
+    {
+      id: 2,
+      title: "Communication Family",
+      description:
+        "Can you explain what spoken expression means, using as many words as possible?",
+    },
+    {
+      id: 3,
+      title: "Communication Family",
+      description:
+        "Can you explain what written communication means, using as many words as possible?",
+    },
+    {
+      id: 4,
+      title: "Communication Family",
+      description:
+        "Can you explain what public speaking means, using as many words as possible?",
+    },
+    {
+      id: 5,
+      title: "Communication Family",
+      description:
+        "Can you explain what negotiation is, using as many words as possible?",
+    },
+    {
+      id: 6,
+      title: "Communication Family",
+      description:
+        "Can you explain what non-verbal communication is, using as many words as possible ?",
+    },
+    {
+      id: 7,
+      title: "Communication Family",
+      description:
+        "Pretend to listen to another player for 1 minute without saying a word. Then summarize what he said. If you forget something, you lose ! (active listening)",
+    },
+    {
+      id: 8,
+      title: "Communication Family",
+      description:
+        "Tell a completely made-up story in 1 minute. You must include a penguin, a cell phone, and a pizza, and it must all make sense. (spoken expression) ",
+    },
+    {
+      id: 9,
+      title: "Communication Family",
+      description:
+        "Present a completely useless product. Make it so attractive that everyone will want to buy it! (public speaking)",
+    },
+    {
+      id: 10,
+      title: "Communication Family",
+      description:
+        "Draw a symbol or icon that represents the negotiation sub-skill in 3 min. Be creative and think of visual elements that illustrate this skill. This could be an object, a character, or a concept that you think represents negotiation well. (negotiation)",
+    },
+    {
+      id: 11,
+      title: "Communication Family",
+      description:
+        "Show a situation where you are trying to convince someone of something. Use only expressions and gestures.(non-verbal communication",
+    },
+
+    // Teamwork Family
+    {
+      id: 12,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what collaboration means, using as many words as possible?",
+    },
+    {
+      id: 13,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what conflict management means, using as many words as possible?",
+    },
+    {
+      id: 14,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what information sharing means, using as many words as possible?",
+    },
+    {
+      id: 15,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what respecting differences means, using as many words as possible?",
+    },
+    {
+      id: 16,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what positive attitude means, using as many words as possible?",
+    },
+    {
+      id: 17,
+      title: "Teamwork Family",
+      description:
+        "Can you explain what constructive feedback means, using as many words as possible?",
+    },
+    {
+      id: 18,
+      title: "Teamwork Family",
+      description:
+        "With the player of your choice, you will have to hold a ball between you and move around the room without it falling. You have 3 minutes to complete the course without dropping it. If you succeed, you win. (collaboration)",
+    },
+    {
+      id: 19,
+      title: "Teamwork Family",
+      description:
+        "Draw what you think is the conflict management icon. Use symbols, shapes, or visuals that you think represent how to resolve conflict. This could be a picture, an object, or even a scene. You have 3 minutes to create your drawing. (conflict management)",
+    },
+    {
+      id: 20,
+      title: "Teamwork Family",
+      description:
+        "You have 3 minutes to convey an important message to another player, but you can only use 5 words! No more. You must be creative to convey as much information as possible with a limited vocabulary. If the player understands your message, you win!    (information sharing)",
+    },
+    {
+      id: 21,
+      title: "Teamwork Family",
+      description:
+        "You have 1 minute to write down 3 differences that you think should be respected in a work environment. Then show them to the other players so they can validate your answers. If you succeed, you win. (Respecting differences)",
+    },
+    {
+      id: 22,
+      title: "Teamwork Family",
+      description:
+        "Draw what you think is an icon of a positive attitude. Use symbols, shapes, or visuals that you think represent how to resolve conflict. This could be a picture, an object, or even a scene. You have 3 minutes to create your drawing. (positive attitude)",
+    },
+    {
+      id: 23,
+      title: "Teamwork Family",
+      description:
+        "Mime to a player of your choice a situation where they did something very well, then mime what could be improved. All without saying a word! Your mission is to make them understand your feedback only through gestures in 1 minute. If the player understands your feedback, you win!  (constructive feedback)",
+    },
+
+    // Problem Solving Family
+    {
+      id: 24,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what critical thinking means, using as many words as possible?",
+    },
+    {
+      id: 25,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what data analysis means, using as many words as possible?",
+    },
+    {
+      id: 26,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what risk management means, using as many words as possible?",
+    },
+    {
+      id: 27,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what decision-making means, using as many words as possible?",
+    },
+    {
+      id: 28,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what stress management means, using as many words as possible?",
+    },
+    {
+      id: 29,
+      title: "Problem Solving Family",
+      description:
+        "Can you explain what resilience means, using as many words as possible?",
+    },
+    {
+      id: 30,
+      title: "Problem Solving Family",
+      description:
+        "Draw a symbol or icon that represents the critical thinking sub-skill in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents critical thinking well.  (critical thinking)",
+    },
+    {
+      id: 31,
+      title: "Problem Solving Family",
+      description:
+        "Your boss asks you to present your sales results, but the only charts you have are made with emojis (😎, 💥, 🍕, etc.). Draw how you would interpret this data to create a clear report and help us understand why you chose these emojis. You have 5 minutes. (Data analysis)",
+    },
+    {
+      id: 32,
+      title: "Problem Solving Family",
+      description:
+        "The team is starting to get stressed because of the workload and tight deadlines. You have 1 minute to propose a solution to reduce this stress and improve productivity, while managing this risk. What do you do? (Risk Management)",
+    },
+    {
+      id: 33,
+      title: "Problem Solving Family",
+      description:
+        "Draw a symbol or icon that represents the decision-making sub-skill, in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents decision-making well. (Decision-making)",
+    },
+    {
+      id: 34,
+      title: "Problem Solving Family",
+      description:
+        " Draw a symbol or icon that represents the stress management sub-skill, in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents stress management well. (Stress management)",
+    },
+    {
+      id: 35,
+      title: "Problem Solving Family",
+      description:
+        "Draw a symbol or icon that represents the resilience sub-skill in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents resilience well. (Resilience) ",
+    },
+
+    // Adaptability Family
+    {
+      id: 36,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what flexibility means, using as many words as possible?",
+    },
+    {
+      id: 37,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what continuous learning means, using as many words as possible?",
+    },
+    {
+      id: 38,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what change management means, using as many words as possible?",
+    },
+    {
+      id: 39,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what open-mindedness means, using as many words as possible?",
+    },
+    {
+      id: 40,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what versatility means, using as many words as possible?",
+    },
+    {
+      id: 41,
+      title: "Adaptability Family",
+      description:
+        "Can you explain what intellectual curiosity means, using as many words as possible?",
+    },
+    {
+      id: 42,
+      title: "Adaptability Family",
+      description:
+        "You just got an urgent notification: a colleague asks you to help him finalize a presentation, but your spilled coffee and lunch are already on your desk. In 1 minute, make a quick mime where you solve these 3 problems at the same time without losing your mind! (flexibility)",
+    },
+    {
+      id: 43,
+      title: "Adaptability Family",
+      description:
+        "Draw a picture of what you imagine a “teachable moment” at work to be: is it a room full of books, a collaborative workshop or maybe a robot teacher? Be creative and think of original elements! You have 3 minutes.  (Continuous learning) ",
+    },
+    {
+      id: 44,
+      title: "Adaptability Family",
+      description:
+        " Draw an office scene before and after a big change. Show how everyone adapts to the change using visual elements. You have 3 minutes to complete your drawing (change management) ",
+    },
+    {
+      id: 45,
+      title: "Adaptability Family",
+      description:
+        "Draw a symbol or icon that represents the Open-Mindedness sub-skill in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents Open-Mindedness well. (Open-mindedness) ",
+    },
+    {
+      id: 46,
+      title: "Adaptability Family",
+      description:
+        "You have been assigned 3 tasks at once: answer a phone call, participate in an online meeting, and respond to an urgent email. Mime with only gestures how you will juggle these 3 tasks without panicking and laughing. You have 1 minute. (Versatility)  ",
+    },
+    {
+      id: 47,
+      title: "Adaptability Family",
+      description:
+        "Draw a symbol or icon that represents the Intellectual Curiosity sub-skill in 3 minutes. Be creative and think of visuals that illustrate this skill. This could be an object, a character, or a concept that you think represents Intellectual Curiosity well. (Intellectual curiosity)  ",
+    },
+
+    // Leadership Family
+    {
+      id: 25,
+      title: "Leadership Family",
+      description:
+        "Can you explain what team motivation means, using as many words as possible?",
+    },
+    {
+      id: 26,
+      title: "Leadership Family",
+      description:
+        "Can you explain what delegation means, using as many words as possible?",
+    },
+    {
+      id: 27,
+      title: "Leadership Family",
+      description:
+        "Can you explain what strategic vision means, using as many words as possible?",
+    },
+    {
+      id: 28,
+      title: "Leadership Family",
+      description:
+        "Can you explain what it means to take initiative, using as many words as possible?",
+    },
+    {
+      id: 29,
+      title: "Leadership Family",
+      description:
+        "Can you explain what coaching means, using as many words as possible?",
+    },
+    {
+      id: 30,
+      title: "Leadership Family",
+      description:
+        "Can you explain what performance management means, using as many words as possible?",
+    },
+
+    // Emotional Intelligence Family
+    {
+      id: 31,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what empathy means, using as many words as possible?",
+    },
+    {
+      id: 32,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what self-awareness means, using as many words as possible?",
+    },
+    {
+      id: 33,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what self-regulation means, using as many words as possible?",
+    },
+    {
+      id: 34,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what social skills mean, using as many words as possible?",
+    },
+    {
+      id: 35,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what managing emotions means, using as many words as possible?",
+    },
+    {
+      id: 36,
+      title: "Emotional Intelligence Family",
+      description:
+        "Can you explain what interpersonal conflict resolution means, using as many words as possible?",
+    },
+
+    // Creativity Family
+    {
+      id: 37,
+      title: "Creativity Family",
+      description:
+        "Can you explain what innovative thinking means, and describe it using as many words as possible?",
+    },
+    {
+      id: 38,
+      title: "Creativity Family",
+      description:
+        "Can you explain what brainstorming means and describe it using as many words as possible?",
+    },
+    {
+      id: 39,
+      title: "Creativity Family",
+      description:
+        "Can you explain what calculated risk-taking means, using as many words as possible?",
+    },
+    {
+      id: 40,
+      title: "Creativity Family",
+      description:
+        "Can you explain what adapting ideas means, using as many words as possible?",
+    },
+    {
+      id: 41,
+      title: "Creativity Family",
+      description:
+        "Can you explain what creative problem solving means, using as many words as possible?",
+    },
+    {
+      id: 42,
+      title: "Creativity Family",
+      description:
+        "Can you explain what design thinking means, using as many words as possible?",
+    },
+  ];
